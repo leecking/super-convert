cb53df0ba31bc4d04d298d69e093799ab1f641c8
leecking <101170636+leecking@users.noreply.github.com>
2026-06-17 21:44:10 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+var C="toolbox-v1",U=["/","/manifest.json","/icon-192.svg","/icon-512.svg"];
+self.addEventListener("install",function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(U);}).then(self.skipWaiting()));});
+self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==C;}).map(function(k){return caches.delete(k);}));}).then(self.clients.claim()));});
+self.addEventListener("fetch",function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request).then(function(res){if(e.request.method==="GET")caches.open(C).then(function(c){c.put(e.request,res.clone());});return res;}).catch(function(){return caches.match("/");});}));});
